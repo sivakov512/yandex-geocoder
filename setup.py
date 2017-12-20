8dcda2eb2cc13aa1d298ea4e2f11613f9ea9c461
Nikita CryptoManiac Sivakov <cryptomaniac.512@gmail.com>
2017-12-20 17:38:20 +1000

    Project configuration

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    author='Nikita Sivakov',
+    author_email='cryptomaniac.512@gmail.com',
+    description=(
+        'Simple library for getting address coordinates via Yandex maps'),
+    install_requires=['requests'],
+    keywords='markdown yaml meta metadata',
+    license='MIT',
+    long_description_markdown_filename='README.md',
+    name='yamaps_coords',
+    packages=['yamaps_coords'],
+    python_requires='>=3.5',
+    setup_requires=['setuptools-markdown'],
+    url='https://github.com/cryptomaniac512/yamaps-coords',
+    version='0.0.1',
+)
