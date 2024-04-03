cbec32d6def22f3e99d9baa35d9d7e9c0a51070f
Nikita Sivakov <sivakov512@icloud.com>
2024-04-03 22:34:32 +0200

    Make linter happy

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 import requests_mock
 
 
-@pytest.fixture()
+@pytest.fixture
 def mock_api() -> typing.Any:
     def _encode(geocode: str, api_key: str = "well-known-key") -> str:
         params = {"format": "json", "apikey": api_key, "geocode": geocode}
@@ -26,7 +26,7 @@ def load_fixture(fixture_name: str) -> dict[str, typing.Any]:
         return json.load(fixture)  # type: ignore
 
 
-@pytest.fixture()
+@pytest.fixture
 def mock_client_response(mocker: typing.Any) -> typing.Any:
     def _mock(fixture_name: str) -> typing.Any:
         with open("./tests/fixtures/{}.json".format(fixture_name)) as fixture:
